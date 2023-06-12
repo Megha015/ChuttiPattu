41f9da9dc3967cef53ef392816216aed5ffebb3d
megala <megha.krishna15@gmail.com>
2023-06-11 21:00:36 -0400

    changed media queries

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import { urlFor } from "../lib/client";
 
 const FooterBanner = ({ footerBanner }) => {
@@ -7,18 +6,12 @@ const FooterBanner = ({ footerBanner }) => {
     <div className="footer-banner-container">
       <div className="banner-desc">
         <div className="left">
-          {/* <p>{footerBanner.discount}</p> */}
           <h3>{footerBanner.largeText2}</h3>
-          {/* <h1>{footerBanner.largeText1}</h1> */}
         </div>
 
         <div className="right">
           <p>{footerBanner.smallText}</p>
           <p>{footerBanner.discount}</p>
-
-          {/* <Link href={`/product/${footerBanner.product}`}>
-            <button type="button">{footerBanner.buttonText}</button>
-          </Link> */}
         </div>
       </div>
       <img src={urlFor(footerBanner.image)} className="footer-banner-image" />
